fdaa7a937acb53ff25cced858c558555f1b38075
SivakumarRajagopal <sivakumar07civil@gmail.com>
2024-01-30 16:46:25 +0530

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+        <div className="footer__container container">
+            <h1 className="footer__title">Sivakumar</h1>
+
+            <ul className="footer__list">
+                <li>
+                    <a href="#about"
+                     className="footer__link">About</a>
+                </li>
+
+                <li>
+                    <a href="#portfolio"
+                     className="footer__link">Project</a>
+                </li>
+
+                <li>
+                    <a href="#testimonial" className="footer__link">Testimonial</a>
+                </li>
+            </ul>
+
+            <div className="footer__social">
+                <a href="mailto:sivakumar07civil@gmail.com" className="footer__social-link" target="_blank" rel="noreferrer">
+                <i class='bx bxl-gmail' ></i>
+                </a>
+
+                <a href="https://www.linkedin.com/in/sivakumar140798/" className="footer__social-link" target="_blank" rel="noreferrer">
+                <i class='bx bxl-linkedin' ></i>
+                </a>
+
+                <a href="https://github.com/Siva07civil" className="footer__social-link" target="_blank" rel="noreferrer">
+                <i class='bx bxl-github' ></i>
+                </a>
+            </div>
+
+            <span className="footer__copy">
+                All rights reserved
+            </span>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+const Social = () => {
+  return (
+    <div>
+      <div className="home__social">
+        <a href="mailto:sivakumar07civil@gmail.com" className="home__social-icon" target="_blank" rel="noreferrer">
+        <i class="uil uil-envelope-alt"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/sivakumar140798/" className="home__social-icon" target="_blank" rel="noreferrer">
+        <i class="uil uil-linkedin-alt"></i>
+        </a>
+
+        <a href="https://github.com/SivakumarRajagopal" className="home__social-icon" target="_blank" rel="noreferrer">
+        <i class="uil uil-github-alt"></i>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Social
